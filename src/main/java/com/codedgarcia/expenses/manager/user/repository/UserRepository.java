258c3b9a07c0e440235952798273f83d68ddaadd
David Garcia <286995159+Code-D-Garcia@users.noreply.github.com>
2026-06-08 13:28:47 -0500

    feat(user): add 🍊 user module

---
+++ src/main/java/com/codedgarcia/expenses/manager/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.codedgarcia.expenses.manager.user.repository;
+
+
+import com.codedgarcia.expenses.manager.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsernameIgnoreCase(String username);
+    Optional<User> findById(Long id);
+}
