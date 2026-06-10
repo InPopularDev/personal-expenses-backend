2267fb32b9fb3a237f254eecda3aff8bbd706ce2
David Garcia <286995159+Code-D-Garcia@users.noreply.github.com>
2026-06-10 09:20:03 -0500

    feat(category): add endpoints in CategoryController

--- src/main/java/com/codedgarcia/expenses/manager/category/controller/CategoryController.java
+++ src/main/java/com/codedgarcia/expenses/manager/category/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.codedgarcia.expenses.manager.category.controller;
 
 import com.codedgarcia.expenses.manager.category.dto.CategoryResponse;
 import com.codedgarcia.expenses.manager.category.dto.CreateCategoryRequest;
+import com.codedgarcia.expenses.manager.category.dto.UpdateCategoryRequest;
+import com.codedgarcia.expenses.manager.category.entity.Type;
 import com.codedgarcia.expenses.manager.category.service.CategoryServiceImpl;
 import com.codedgarcia.expenses.manager.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -23,8 +25,40 @@ public class CategoryController {
     }
 
     @PostMapping
-    public CategoryResponse addCategory(@AuthenticationPrincipal User user, @RequestBody CreateCategoryRequest request) {
+    public CategoryResponse addCategory(
+            @AuthenticationPrincipal User user,
+            @RequestBody CreateCategoryRequest request) {
         return categoryService.createCategory(user.getId(), request);
+    }
+
+    @GetMapping
+    public List<CategoryResponse> getAllCategoriesByType(
+            @AuthenticationPrincipal User user,
+            @RequestParam Type type) {
+        return categoryService.getCategoriesByType(user.getId(), type);
+    }
+
+    @GetMapping
+    public CategoryResponse getCategory(
+            @AuthenticationPrincipal User user,
+            @RequestParam("categoryId") Long categoryId) {
+        return categoryService.getCategory(user.getId(), categoryId);
+    }
+
+    @PutMapping
+    public CategoryResponse updateCategory(
+            @AuthenticationPrincipal User user,
+            @RequestParam("categoryId") Long categoryId,
+            @RequestBody UpdateCategoryRequest request
+    ){
+        return categoryService.updateCategory(user.getId(), categoryId, request);
+    }
+
+    @DeleteMapping
+    public void deleteCategory(
+            @AuthenticationPrincipal User user,
+            @RequestParam("categoryId") Long categoryId) {
+        categoryService.deleteCategory(user.getId(), categoryId);
     }
 
 }
